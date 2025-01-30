6b01ead7330cd7c052c67b7dd79f7ce6ed6e7f57
Taha-Asad <tahaasad709@gmail.com>
2025-01-30 22:21:31 +0500

    Create userModel.js

---
+++ Backend/models/userModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose" ;
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        require:true , 
+    },
+    userName:{
+        type:String ,
+        require: true ,
+        unique: true
+    },
+    email:{
+        type:String,
+        require: true ,
+        unique: true
+    },
+    password:{
+        type:String ,
+        require:true
+    }
+})
+
+export default mongoose.model('User' , userSchema);
